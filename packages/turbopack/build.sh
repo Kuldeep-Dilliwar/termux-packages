7661677550311f1a413cc576992e308a01e449f8
Kuldeep Dilliwar <kuldeepdilliwar@gmail.com>
2026-08-18 23:47:55 +0530

    Update build script with additional RUSTFLAGS and CFLAGS

--- packages/turbopack/build.sh
+++ packages/turbopack/build.sh
@@ -19,7 +19,9 @@ termux_step_make() {
 	export RUSTC_BOOTSTRAP=1
 	termux_setup_rust
 	termux_setup_nodejs
-	export RUSTFLAGS="--cfg tokio_unstable"
+	export RUSTFLAGS="--cfg tokio_unstable -C tls-model=initial-exec -Z plt=no"
+	export CFLAGS="$CFLAGS -fno-emulated-tls"
+	export CXXFLAGS="$CXXFLAGS -fno-emulated-tls"
 	local ENV_PREFIX=$(echo "$CARGO_TARGET_NAME" | tr '[:lower:]-' '[:upper:]_')
 	if [ "$TERMUX_ARCH" == "aarch64" ]; then
 		export RUSTFLAGS+=" -Zshare-generics=y -Csymbol-mangling-version=v0"
